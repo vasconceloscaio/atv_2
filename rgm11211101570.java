2fb3b967ac8e94c4f2f7262983e692f01aa0d189
Caio Vasconcelos <100734526+vasconceloscaio@users.noreply.github.com>
2022-03-16 18:51:16 -0300

    Add files via upload

---
+++ rgm11211101570.java
@@ -0,0 +1,10 @@
+public class rgm11211101570 {
+    public static void main(String[] args) {
+        System.out.println("Nome:Caio Santos de Vasconcelos");
+        System.out.println("Rgm:11211101570");
+        System.out.println("Idade:22 anos");
+        System.out.println("github: vasconceloscaio");
+        System.out.println("linkedIn: www.linkedin.com/in/caiovasconcelos1");
+        System.out.println("Curso/Turma: S.I/3o semestre noite");
+    }
+}
